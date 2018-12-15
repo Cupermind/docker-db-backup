84b93b5305429a41d3f200c833838ed98d50ee8f
Alex Koval <alex.v.koval@labster.com>
2018-12-15 16:59:40 +0200

    postgresql11

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT=cupermind/db-backup
-SRCS="postgres:9.4 postgres:9.5 postgres:9.6 postgres:10"
+SRCS="postgres:9.4 postgres:9.5 postgres:9.6 postgres:10 postgres:11"
 SRCS="$SRCS mariadb:10.3"
 SRCS="$SRCS mongo:3.2 mongo:3.4 mongo:3.6"
 for from in $SRCS
